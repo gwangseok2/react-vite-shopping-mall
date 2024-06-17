2f3f07f3acaf966a659b7cb462ebc57a9c253e05
sging <rhrnrkd135@gmail.com>
2024-06-17 23:01:58 +0900

    + 초기 세팅 route 세팅

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,5 @@
+const products = () => {
+  return <div>상품페이지</div>;
+};
+
+export default products;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import withReactRouter from "vite-plugin-next-react-router";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    withReactRouter({
+      pageDir: "src/pages",
+      extensions: ["js", "jsx", "ts", "tsx"],
+      // like '_app' in Next.js, `_document` is not supported since all rendering is done in client side
+      layout: "_layout",
+    }),
+  ],
+});
